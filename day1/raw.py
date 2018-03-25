6f75b5bf41ef7d02c058c2274984a62c492afe7c
maple <929856540@qq.com>
2018-03-25 07:58:06 -0700

    多行输出字符串和转义字符

---
+++ day1/raw.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#加上前缀r表示后面的字符串中的字符就不需要转义了。
+print r'\(~_~)/ \(~_~)/'
+
+#使用... ...表示多行字符串
+print r'''"To be,or not to be":that is question.
+Whether it's nobler in the mind to suffer.'''
+
+
