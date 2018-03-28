a4398010efc9b101d6db6ec2949ab8f8c3d06440
maple <929856540@qq.com>
2018-03-28 07:59:19 -0700

    python的循环结构

---
+++ day4/if.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+age = 20
+if age >= 18:
+    print 'your age is',age
+    print 'adult'
+print 'END'
+
+scode = 75
+if scode >= 60:
+    print 'pass'
+else:
+    print 'failed'
