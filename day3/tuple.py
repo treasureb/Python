5915994e94716c492659dccc953051215d56e6e4
maple <929856540@qq.com>
2018-04-07 19:00:58 -0700

    tuple支持的操作

--- day3/tuple.py
+++ day3/tuple.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 #1.tuple的元素不能改变
+#tuple也支持下标访问,前后均可
 t=('Adam','Lisa','Bart')
 print t
+print t[1]
+print t[-1]
 
 #2.tuple本身的元素不能被修改，但本身元素的值可以被修改
 t1=('a','b',['A','B'])
