8ebaee70c8de5c5afa650f6185f20abd2d3db6cb
maple <929856540@qq.com>
2018-04-08 23:35:36 -0700

    导入函数模块

---
+++ module/importMod.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#导入一个包
+import math
+
+#使用导入的函数
+print math.pow(2,0.5)
+
+#导入一个变量
+math.pi
+
+#单独导入几个函数
+from math import pow,sin,log
+
+#起别名
+from math import log as logger
+
+print logger(10)
+
+
+#os.path模块提供isdir()和isfile()函数
