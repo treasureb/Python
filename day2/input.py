09fe5839e62c38baa574b855530a35720f5c1f5e
treasureb <929856540@qq.com>
2018-10-18 09:22:10 +0800

    input来获取命令行的输入参数,可以直接跟提示语句

---
+++ day2/input.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+number = int(input("Please Enter an integer:"))
+if number < 100:
+    print("Your number is smaller than 100")
+elif number == 100:
+    print("Your number is equal to 100")
+else:
+    print("Youu number is greater than 100")
