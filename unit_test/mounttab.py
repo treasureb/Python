c98db91941c200117865d2c5abe131e749534f8f
treasureb <929856540@qq.com>
2018-10-19 23:26:01 +0800

    python中的单元测试框架

---
+++ unit_test/mounttab.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env
+#coding=utf-8
+
+import os
+
+def parse_mounts():
+    """
+    分析 /proc/mounts 并返回元组的列表
+    """
+    result = []
+    if os.path.exists('/proc/mounts'):
+        fd = open('/proc/mounts')
+        for line in fd:
+            line = line.strip();#删除字符串首尾指定的字符,默认为空格或换行符
+            words = line.split()
+            print('{} on {} type{}'.format(words[0],words[1],words[2]),end=' ')
+            if len(words) > 5:
+                res = (words[0],words[1],words[2],'({})'.format(''.join(words[3:-2])))
+            else:
+                res = (words[0],words[1],words[2])
+            result.append(res)
+        fd.close()
+    return result
+
+def mount_details():
+    """
+    打印挂载详细信息
+    """
+    result = parse_mounts()
+    for line in result:
+        if len(line) == 4:
+            print('{} on {} type {}{}'.format(*line))
+        else:
+            print('{} on {} type {}'.format(*line))
+
+if __name__ == '__main__':
+    mount_details()
