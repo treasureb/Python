cb2021b8cc5fb99414f9ce4023e86f252db898ce
treasureb <929856540@qq.com>
2018-10-19 23:33:27 +0800

    文件操作

---
+++ fileOperator/argv.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env
+#coding=utf-8
+
+import sys
+print("First value",sys.argv[0])
+print("All values")
+for i,x in enumerate(sys.argv):
+    print(i,x)
---
+++ fileOperator/copy.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env
+#coding=utf-8
+
+import sys
+if len(sys.argv) > 3:
+    print("Wrong parameter")
+    print("./copyfile.py file1 file2")
+    sys.exit(1)
+
+f1 = open(argv[1])
+s = f1.read()
+f1.close()
+f2 = open(sys.argv[2],'w')
+f2.write(s)
+f2.close()
